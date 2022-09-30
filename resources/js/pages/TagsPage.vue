cad0adfcb2ae41e12cb451a3abc0af6c948e2306
bigeppo <carlog91cast@gmail.com>
2022-09-30 17:24:51 +0200

    add methods in tagsPage.vue

--- resources/js/pages/TagsPage.vue
+++ resources/js/pages/TagsPage.vue
@@ -5,7 +5,27 @@
 </template>
 <script>
 export default {
-    name:'TagsPage'
+    name:'TagsPage',
+    data: function () {
+        return {
+            tags: [],
+        }
+    },
+    methods: {
+        getTags(tagsPage = 1) {
+            axios.get("/api/tags", {
+                page: tagsPage
+            }).then((response) => {
+                console.log(response.data.results.data);
+                this.tags = response.data.results.data;
+            }).catch((error) => {
+                console.error(error);
+            })
+        }
+    },
+    created() {
+        this.getTags();
+    }
 }
 </script>
 <style>
